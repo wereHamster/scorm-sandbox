e5c0fa06bf4e643b3b3102e83cffc67786934255
Utkarsh Upadhyay <musically.ut@gmail.com>
2014-08-28 16:50:05 +0200

    Allow user to choose which resource to test.

--- scorm-sandbox.hs
+++ scorm-sandbox.hs
@@ -7,13 +7,9 @@ import           Text.XML.HXT.Core                ( readDocument, runX, getAttrV
 import           Text.XML.HXT.XPath               ( getXPathTrees )
 import           Data.FileEmbed                   ( embedFile )
 import           Data.Text.Lazy                   ( pack )
-
--- import           Text.XML.HXT.Arrow.XmlState.SystemConfig
---                                                  ( withValidate
---                                                  , no
---                                                  )
--- import           Text.XML.HXT.Arrow.WriteDocument ( writeDocument )
-import           Control.Arrow                   ( (>>>) )
+import           Safe                             ( readMay )
+import           Control.Arrow                    ( (>>>) )
+import           System.IO                        ( hFlush, stdout, hPutStrLn, stderr )
 
 import qualified System.Environment              as Env
 import qualified System.IO.Temp                  as Temp
@@ -22,16 +18,52 @@ import qualified Codec.Archive.Zip               as Zip
 import qualified Data.ByteString.Lazy            as BL
 import qualified Data.ByteString                 as B
 import qualified System.FilePath                 as Path
--- import qualified Text.XML.HXT.DOM.ShowXml        as Xml
 import qualified Data.Text.Lazy.Encoding         as TE
 
 import qualified Web.Scotty                      as Scotty
 import qualified Network.Wai.Middleware.Static   as Static
 
+logErrLn :: String -> IO ()
+logErrLn = hPutStrLn stderr
+
 printHelp :: IO ()
 printHelp = do
     progName <- Env.getProgName
-    putStrLn $ "Usage: " ++ progName ++ " <SCORM_archive.zip>"
+    logErrLn $ "Usage: " ++ progName ++ " <SCORM_archive.zip>"
+
+
+chooseTarget :: [String] -> IO (Maybe String)
+chooseTarget validHrefs =
+        if length validHrefs > 1 then do
+            putStrLn "Possible targets: "
+            putStrLn "~~~~~~~~~~~~~~~~~~\n"
+
+            forM_ (zip [(1::Int)..] validHrefs) $ \(idx, href) -> do
+                putStrLn $ (show idx) ++ ") " ++ href
+
+            putStr "Choose one of the targets to test:"
+            hFlush stdout
+
+            mbChosenIdx <- readMay `fmap` getLine
+
+            case mbChosenIdx of
+                Nothing -> do
+                    putStrLn "\n\nPlease enter a number.\n\n"
+                    chooseTarget validHrefs
+
+                Just chosenIdx -> do
+                    let chosenHref = validHrefs !! chosenIdx
+                    putStrLn $ "Chose Href: " ++ chosenHref
+
+                    return $ Just chosenHref
+        else if length validHrefs == 1 then do
+            let chosenHref = head validHrefs
+            putStrLn $ "\n\nOnly one resource: " ++ chosenHref ++ "\n\n"
+            return $ Just chosenHref
+        else do
+            logErrLn $ "\n\nNo valid resource to choose from.\n\n"
+            return Nothing
+
 
 main :: IO ()
 main = do
@@ -45,7 +77,7 @@ main = do
     let scormPgkName = head args
     pkgExists <- Dir.doesFileExist scormPgkName
     unless pkgExists $ do
-        putStrLn $ "File: " ++ scormPgkName ++ " does not exist"
+        logErrLn $ "File: " ++ scormPgkName ++ " does not exist"
         exitFailure
 
     zipArch <- Zip.toArchive `fmap` (BL.readFile $ head args)
@@ -55,7 +87,7 @@ main = do
         let imsManifestFilePath = Path.joinPath [tmpDir, "imsmanifest.xml"]
         manifestExists <- Dir.doesFileExist imsManifestFilePath
         unless manifestExists $ do
-            putStrLn $ "The manifest file doesn't exist, not a valid SCORM package."
+            logErrLn $ "The manifest file doesn't exist, not a valid SCORM package."
             -- TODO (UU): Does this do proper clean up with withSystemTempDirectory?
             exitFailure
 
@@ -67,21 +99,27 @@ main = do
             >>> getAttrValue "href"
 
         let validHrefs = filter (/= "") resourceHrefs
+        mbChosenHref <- chooseTarget validHrefs
 
-        putStrLn "Hrefs: "
-        forM_ (zip [(1::Int)..] validHrefs) $ \(idx, href) -> do
-            putStrLn $ (show idx) ++ ") " ++ href
+        case mbChosenHref of
+            Nothing -> do
+                logErrLn $ "No valid HTML resource to test."
+                exitFailure
+            Just chosenHref -> do
 
-        -- Present the choice to user
-        let chosenHref = validHrefs !! 0
+                Scotty.scotty 8000 $ do
+                    Scotty.get "/" $ Scotty.redirect "/index.html"
 
-        Scotty.scotty 8000 $ do
-            Scotty.get "/" $ Scotty.redirect "/index.html"
-            Scotty.get "/index.html"       $ Scotty.html $ TE.decodeUtf8 $ BL.fromStrict indexHtml
-            Scotty.get "/scorm-sandbox.js" $ Scotty.text $ TE.decodeUtf8 $ BL.fromStrict scormSandboxJs
-            Scotty.get "/content.html"     $ Scotty.redirect $ pack chosenHref
+                    Scotty.get "/index.html" $
+                        Scotty.html $ TE.decodeUtf8 $ BL.fromStrict indexHtml
 
-            Scotty.middleware $ Static.staticPolicy $ Static.addBase tmpDir
+                    Scotty.get "/scorm-sandbox.js" $
+                        Scotty.text $ TE.decodeUtf8 $ BL.fromStrict scormSandboxJs
+
+                    Scotty.get "/content.html" $
+                        Scotty.redirect $ pack chosenHref
+
+                    Scotty.middleware $ Static.staticPolicy $ Static.addBase tmpDir
 
 
 indexHtml :: B.ByteString
